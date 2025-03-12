b4cef8ee58e4cfbc994f6358e49e35e265f1cf83
00ffcc <yanqiwanrenhe@qq.com>
2025-03-12 13:51:58 +0800

    modified:   main_grpo.sh 	new file:   sft/filter.py 	new file:   sft/length_distribution.png 	modified:   verl/trainer/config/ppo_trainer.yaml

---
+++ sft/filter.py
@@ -0,0 +1,16 @@
+import json
+
+with open('count_sft_data_30.json') as f:
+    data = json.load(f)
+
+length = []
+for i in data:
+    length.append(len(i['output']))
+
+# 画出length的频次分布
+import matplotlib.pyplot as plt
+plt.hist(length, bins=100, range=(0, 10000))
+plt.xlabel('length')
+plt.ylabel('frequency')
+plt.title('length distribution')
+plt.savefig('length_distribution.png')
